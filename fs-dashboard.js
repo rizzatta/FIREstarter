975fa9a9bb953b5e12981460b764e8a2b8100b83
rizzatta <rizzacolis18@gmail.com>
2026-05-11 00:11:16 +0800

    Added Box-Muller Transform to generate random numbers based on a bell curve

--- fs-dashboard.js
+++ fs-dashboard.js
@@ -330,6 +330,14 @@ async function updateDatabase() {
     }
 }
 
+// BOX-MULLER TRANSFORM
+function randomNormal(mean, stdDev) {
+    let u1 = Math.random();
+    let u2 = Math.random();
+    let z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2.0 * Math.PI * u2);
+    return mean + z0 * stdDev;
+}
+
 // LOGOUT
 function logout() {
     localStorage.removeItem('activeUserId');
